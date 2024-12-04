a7ac35840528ec0c35b74a6778bdd85b003d1e96
cuitingzhao@hotmail.com <81788008+TZ-Cui@users.noreply.github.com>
2024-12-04 23:40:45 +0800

    解决手机上要开始录音才开始请求网络权限的问题

--- lib/pages/splash/controller.dart
+++ lib/pages/splash/controller.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
+import 'package:dio/dio.dart';
 import '../../utils/noise_analyzer.dart';
 import '../../core/errors/error_handler.dart';
 import '../../core/routes/app_pages.dart';
+import '../../core/config/api_config.dart';
 
 class SplashController extends GetxController {
   // 观察变量
   final _isAnalyzing = true.obs;
   final _environmentDescription = '正在检测环境噪音...'.obs;
+  final _dio = Dio();
 
   // Getters
   bool get isAnalyzing => _isAnalyzing.value;
@@ -16,6 +19,8 @@ class SplashController extends GetxController {
   void onInit() {
     super.onInit();
     _startNoiseAnalysis();
+    // 同时预热网络连接
+    _warmupNetwork();
   }
 
   Future<void> _startNoiseAnalysis() async {
@@ -37,6 +42,16 @@ class SplashController extends GetxController {
       _environmentDescription.value = NoiseAnalyzer.getEnvironmentDescription();
     } finally {
       _isAnalyzing.value = false;
+    }
+  }
+
+  // 预热网络连接
+  Future<void> _warmupNetwork() async {
+    try {
+      final baseUrl = APIConfig.current.environment.baseURL;
+      await _dio.get(baseUrl);
+    } catch (e) {
+      // 忽略预热失败
     }
   }
 }
